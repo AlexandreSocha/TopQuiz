fd16277a686b76981fe65e082668927febf14fc3
alexa <alexandre.socha1994@gmail.com>
2023-02-09 19:38:26 +0100

    on continue

--- app/src/main/java/com/alexcorp/topquiz/controller/GameActivity.java
+++ app/src/main/java/com/alexcorp/topquiz/controller/GameActivity.java
@@ -7,6 +7,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.alexcorp.topquiz.R;
+import com.alexcorp.topquiz.model.Question;
+import com.alexcorp.topquiz.model.QuestionBank;
+
+import java.util.Arrays;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -15,6 +19,9 @@ public class GameActivity extends AppCompatActivity {
     private Button mReponse2Button;
     private Button mReponse3Button;
     private Button mReponse4Button;
+
+    private QuestionBank mQuestionBank = generateQuestions();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,5 +31,46 @@ public class GameActivity extends AppCompatActivity {
         mReponse2Button.findViewById(R.id.game_activity_button_2);
         mReponse3Button.findViewById(R.id.game_activity_button_3);
         mReponse4Button.findViewById(R.id.game_activity_button_4);
+    }
+
+    private QuestionBank generateQuestions(){
+        Question question1 = new Question(
+                "Who is the creator of Android?",
+                Arrays.asList(
+                        "Andy Rubin",
+                        "Steve Wozniak",
+                        "Jake Wharton",
+                        "Paul Smith"
+                ),
+                0
+        );
+
+        Question question2 = new Question(
+                "When did the first man land on the moon?",
+                Arrays.asList(
+                        "1958",
+                        "1962",
+                        "1967",
+                        "1969"
+                ),
+                3
+        );
+
+        Question question3 = new Question(
+                "What is the house number of The Simpsons?",
+                Arrays.asList(
+                        "42",
+                        "101",
+                        "666",
+                        "742"
+                ),
+                3
+        );
+
+        return new QuestionBank(Arrays.asList(question1, question2, question3));
+    }
+
+    private void displayQuestion(final Question question) {
+        // Set the text for the question text view and the four buttons
     }
 }
--- app/src/main/java/com/alexcorp/topquiz/model/QuestionBank.java
+++ app/src/main/java/com/alexcorp/topquiz/model/QuestionBank.java
@@ -1,19 +1,26 @@
 package com.alexcorp.topquiz.model;
 
+import java.io.Serializable;
+import java.util.Collections;
 import java.util.List;
 
-public class QuestionBank {
+public class QuestionBank implements Serializable {
 
-    private List<Question> mQuestionList;
-    private int mNextQuestionIndex;
+    private final List<Question> mQuestionList;
+    private int mQuestionIndex;
 
     public QuestionBank(List<Question> questionList) {
-        // Shuffle the question list before storing it
+        mQuestionList = questionList;
+        Collections.shuffle(mQuestionList);
     }
 
-    public void getNextQuestion() {
-        // Loop over the questions and return a new one at each call
-        // renverra un type Question
+    public Question getCurrentQuestion() {
+        return mQuestionList.get(mQuestionIndex);
+    }
+
+    public Question getNextQuestion() {
+        mQuestionIndex++;
+        return getCurrentQuestion();
     }
 }
 
